fb74028054acb4df4e28e83a8139720cf2f9a193
rachaelbardell <rachael.bardell@gmail.com>
2013-08-19 14:07:06 -0500

    cleaned up version

--- create_model.R
+++ create_model.R
@@ -3,6 +3,8 @@ library(stringr)
 
 setwd("~/Documents/test_FP_models/Google_API")
 
+# Cory's code to clean the text and use textcat to classify new tweets
+
 clean.text <- function(text){
   # INPUT: Text to be "cleansed"
   # OUTPUT: Cleansed text
@@ -46,7 +48,7 @@ clean.text <- function(text){
 }  
 
 # read the manually classified tweets
-df <- read.csv("/home/rachael/Documents/test_FP_models/fp_tweets.csv", stringsAsFactors=F)
+df <- read.csv("~/Desktop/test_FP_models/fp_tweets.csv", stringsAsFactors=F)
 
 # do the preprocessing to the data. this needs to be done before any prediction
 # using the model that is created.
@@ -63,62 +65,63 @@ textcat("i have the food poisoning", fp.model)
 #---------------------------------------------------------------------
 #---------------------------------------------------------------------
 
-# train the model with 70% of the manually classified tweets
-sample2 <- sample(1:nrow(df), 0.70*nrow(df))
+# My code to train/test new models
 
-train2 <- df[sample2,]
-pred2 <- df[-sample2,]
+# train the model with 75% of the manually classified tweets
+sample <- sample(1:nrow(df), 0.70*nrow(df))
 
-fp.model <- textcat_profile_db(train2$text.cleansed, train2$manual_class)
+train <- df[sample,]
+pred <- df[-sample,]
 
-# use the remaining 30% of the manually classified tweets for prediction
-pred_class2 <- sapply(pred2$text.cleansed, function(x)textcat(x, fp.model))
+fp.model <- textcat_profile_db(train$text.cleansed, train$manual_class)
 
-pred2$pred_class2 <- pred_class2
+# use the remaining 25% of the manually classified tweets for prediction
+pred <- sapply(pred$text.cleansed, function(x)textcat(x, fp.model))
 
 #-------------------------------------------------------------------------
 # Google Prediction API
+# data to be exported and used in Google Prediction API
 export <- subset(df, select=c("manual_class, text.cleansed"))
 
+# prediction works best if the factors are not integers
 export$manual_class[export$manual_class == "1"] <- "T"
 export$manual_class[export$manual_class == "0"] <- "F"
 
-write.csv(export, file = 'exported_FP.csv', row.names = F, col.names=FALSE)
-
-# train the Google prediction API with the same 70% as we did with the fp.model
-# pred the same remaining 30% with the google prediction API and compare
+# train the Google prediction API with the same 75% as we did with the fp.model
+# pred the same remaining 25% with the google prediction API and compare
 # make sure there are no column or row names, and that the manual_class column is first
-export_train <- export[sample2,]
-export_pred <- export[-sample2,]
+export_train <- export[sample,]
+export_pred <- export[-sample,]
 write.csv(export_train, file = 'exported_train_FP.csv', row.names = F, col.names=FALSE)
 write.csv(export_pred, file = 'exported_pred_FP.csv', row.names = F, col.names=FALSE)
 
-# results from GoogleAPI
+# import results from GoogleAPI
 GoogleAPI <- read.csv("/home/rachael/Documents/test_FP_models/Google_API/exported_pred_FP.csv", stringsAsFactors=F, header = F)
 
-stats <- function(data, pred, man){
+stats <- function(predictions, manual_class){
   # pred is the column name for the column of predicted classifications
   # man is the column name for the manual classified column
-  data$Accuracy[data$man == "1" & data$pred == "1"] <- "a"
-  data$Accuracy[data$man == "0" & data$pred == "0"] <- "b"
-  data$Accuracy[data$man == "1" & data$pred == "0"] <- "c"
-  data$Accuracy[data$man == "0" & data$pred == "1"] <- "d"
+  
+  # following the labels of confusionMatrix from the caret package
+  # return the number of TRUE values
+  A <- length(which(manual_class == "1" & predictions == "1"))
+  B <- length(which(manual_class == "0" & predictions == "1"))
+  C <- length(which(manual_class == "1" & predictions == "0"))
+  D <- length(which(manual_class == "0" & predictions == "0"))
   # Type I error: food poisoning tweet that model prediction classifies as 0
   # Type II error: non-food poisoning tweet that model prediction classifies as 1
-  # a and b are correct classifications
-  # c is false negatives and d is false postivies
-  accuracy <- table(data$Accuracy)
-  error <- (accuracy[3] + accuracy[4]) / sum(accuracy)
-  accuracy_rate <- (accuracy[1]+accuracy[2]) / sum(accuracy)
- 
-  sensitivity <- accuracy[1] / (accuracy[1] + accuracy[3])
-  specificity <- accuracy[2] / (accuracy[2] + accuracy[4])
-
-  data_stats <- c(accuracy_rate, error, sensitivity, specificity)
-  names(data_stats) <- c("Accuracy Rate", "Error Rate", "Sensitivity","Specificity") 
-  return(data_stats)
+  # A and D are correct classifications
+  # C is false negatives and B is false postivies
+  
+  Accuracy <- (A + D) / (A + B + C + D) # Accuracy
+  Sensitivity <- A / (A + C) # Sensitivity
+  Specificity <- D / (D + B) # Specificity
+  stats <- list(Accuracy, Sensitivity, Specificity)
+  names(stats) <- c("Accuracy", "Sensitivity", "Specificity")
+  
+  return(stats)
 }
 
-stats_fp.model <- stats(pred2, manual_class, pred_class_2)
+stats_fp.model <- stats(pred, manual_class)
 # .7209 Accuracy, .7778 Sensitivity, .6667 Specificity
-stats_Google <- stats(Google_API, V1, V2)
+stats_Google <- stats(V1, V2)
